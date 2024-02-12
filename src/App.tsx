6d3efb08fa7371b3c0ba0b755428164a7e0c6d45
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-02-12 18:17:33 +0530

    selected font face and added some static elements and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Machine Coding</h1>
+    <div className='flex h-screen bg-slate-600'>
+      <div className='flex flex-col gap-5 p-5 bg-slate-800'>
+        <div className='flex items-center justify-center w-10 h-10 rounded-full cursor-pointer bg-slate-700'>
+          1
+        </div>
+        <div className='flex items-center justify-center w-10 h-10 text-center rounded-full cursor-pointer bg-slate-700'>
+          2
+        </div>
+        <div className='flex items-center justify-center w-10 h-10 text-center rounded-full cursor-pointer bg-slate-700'>
+          3
+        </div>
+        <div className='flex items-center justify-center w-10 h-10 text-center rounded-full cursor-pointer bg-slate-700'>
+          4
+        </div>
+        <div className='flex items-center justify-center w-10 h-10 text-center rounded-full cursor-default ring-2 ring-slate-400 bg-slate-700'>
+          5
+        </div>
+        <div className='flex items-center justify-center w-10 h-10 text-center rounded-full cursor-pointer bg-slate-700'>
+          6
+        </div>
+      </div>
+      <div className='bg-slate-900 grow'>main page</div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
   },
   plugins: [],
 };
