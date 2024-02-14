8b7e02b5fcdc8a749dcf61f4cd5c68a2c021a948
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-02-14 22:43:16 +0530

    placed flie link at bottom of code box

--- src/components/FetchCode.tsx
+++ src/components/FetchCode.tsx
@@ -9,24 +9,26 @@ export default function FetchCode({
   code: string;
   completedOn: string;
 }) {
+  console.log("id", id, completedOn);
+
   return (
     <div>
-      <code className='relative block p-5 m-10 rounded-lg bg-slate-950 border-slate-400 border-[1px] w-[80%]'>
+      <code className='relative block p-5 pr-0 mb-0  m-10 rounded-lg bg-slate-950 border-slate-400 border-[1px] w-[80%]'>
         <pre className='overflow-y-scroll text-sm whitespace-break-spaces max-h-[500px]'>
           {code}
         </pre>
         <CopyButton data={code} />
+        <a
+          href={`https://github.com/syedmhdm/machine-coding/blob/main/src/appdata/${id}/Solution.tsx`}
+          target='_blank'
+          className='absolute bottom-0 text-xs right-1 text-slate-400 hover:underline'
+        >
+          file link
+        </a>
       </code>
       {completedOn !== "" ? (
-        <div className='absolute inset-x-0 flex justify-between gap-1 text-xs right-1 top-1 text-slate-400'>
-          <a
-            href={`https://github.com/syedmhdm/machine-coding/blob/main/src/appdata/${id}/Solution.tsx`}
-            target='_blank'
-            className='z-20 self-end text-xs text-slate-400 hover:underline'
-          >
-            file link
-          </a>
-          <p>Completed On: {completedOn}</p>
+        <div className='absolute justify-between text-xs right-1 top-1 text-slate-400'>
+          <p>{completedOn}</p>
         </div>
       ) : null}
     </div>
