f09954630a22b4872899501655a6107d1f6cb0b9
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-02-22 11:28:20 +0530

    first question completed

--- src/appdata/1/Solution.tsx
+++ src/appdata/1/Solution.tsx
@@ -1,3 +1,106 @@
+import { useEffect, useState } from "react";
+
 export default function Solution() {
-  return <div>Solution One</div>;
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [secs, setSecs] = useState(0);
+  const [mins, setMins] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(
+    function () {
+      if (!isRunning) return;
+      if (mins === 0 && secs === 0) return;
+      const interval = setInterval(function () {
+        setSecs((prevSecs) => {
+          if (prevSecs > 0) return prevSecs - 1;
+          if (prevSecs === 0 && mins > 0) return 59;
+          return 0;
+        });
+        setMins((prevMins) => {
+          // if(secs === 0 && prevMins === 0) return 0
+          if (secs === 0) return prevMins - 1;
+          return prevMins;
+        });
+      }, 1000);
+      return () => clearInterval(interval);
+    },
+    [isRunning, mins, secs]
+  );
+
+  function handleMinutesChange(e) {
+    setMinutes(e.target.value);
+  }
+  function handleSecondsChange(e) {
+    setSeconds(e.target.value);
+  }
+  function handleStartClick() {
+    if (mins !== 0 && secs !== 0) return;
+
+    if (seconds > 60) {
+      setSecs(seconds % 60);
+      setMins((prev) => prev + Math.floor(seconds / 60));
+    } else {
+      setSecs(seconds);
+      setMins(minutes);
+    }
+
+    setSeconds(0);
+    setMinutes(0);
+    setIsRunning(true);
+  }
+
+  function handlePlayPauseClick() {
+    setIsRunning((prev) => !prev);
+  }
+  function handleResetClick() {
+    setSeconds(0);
+    setMinutes(0);
+    setSecs(0);
+    setMins(0);
+    setIsRunning(false);
+  }
+
+  const displayTimer = `${
+    String(mins).length === 1
+      ? `0${mins}`
+      : `${String(mins).length === 0 ? "00" : mins}`
+  }:${
+    String(secs).length === 1
+      ? `0${secs}`
+      : `${String(secs).length === 0 ? "00" : secs}`
+  }  `;
+
+  return (
+    <>
+      <div className='flex gap-1'>
+        <input
+          placeholder='minutes'
+          type='number'
+          value={minutes}
+          onChange={handleMinutesChange}
+        />
+        <input
+          placeholder='seconds'
+          type='number'
+          value={seconds}
+          onChange={handleSecondsChange}
+        />
+      </div>
+      <br />
+      <div className='flex gap-1'>
+        <button className='bg-slate-300' onClick={handleStartClick}>
+          Start
+        </button>
+        <button className='bg-slate-300' onClick={handlePlayPauseClick}>
+          Play/Pause
+        </button>
+        <button className='bg-slate-300' onClick={handleResetClick}>
+          Reset
+        </button>
+      </div>
+      <br />
+      <div>{displayTimer}</div>
+    </>
+  );
 }
